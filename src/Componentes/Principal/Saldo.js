dd4bd03a68159e22262e61b09eae549672b6347d
=Jean Moreira <jeanreal099@gmail.com>
2022-04-03 23:42:05 -0300

    ToDo saldo

--- src/Componentes/Principal/Saldo.js
+++ src/Componentes/Principal/Saldo.js
@@ -2,7 +2,21 @@ import React from 'react'
 
 class Saldo extends React.Component {
   render() {
-    return <div>Saldo: R${String(this.props.saldo).replace('.', ',')}</div>
+    if (this.props.lista.tipo == 'Recebimento') {
+      valor = valor + currentItem.valor
+    } else {
+      valor = valor - currentItem.valor
+    }
+
+    const saldoFinal = this.props.lista.reduce((valor, currentItem) => {
+      if (currentItem.tipo == 'Recebimento') {
+        valor = valor + currentItem.valor
+      } else {
+        valor = valor - currentItem.valor
+      }
+    })
+
+    return <div>Saldo: R${String(saldoFinal).replace('.', ',')}</div>
   }
 }
 
